47c08bdb4306bbfcedbe0d185a2ea02fcf9f73a2
xiujianying <xiujianying@xywy.com>
2016-10-09 14:57:39 +0800

    11

--- controllers/WxController.php
+++ controllers/WxController.php
@@ -33,104 +33,104 @@ class WxController extends Controller
     public function reponseMsg()
     {
         //1.获取到微信推送过来post数据（xml格式）
-        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        $postArr = isset($GLOBALS['HTTP_RAW_POST_DATA'])?$GLOBALS['HTTP_RAW_POST_DATA']:'';
         //2.处理消息类型，并设置回复类型和内容
-        $ser = serialize($postArr);
+        $ser ='111'.serialize($postArr);
         Yii::$app->db->createCommand("insert into datas(`data`) values('$ser')")->execute();
-
-        $postObj = simplexml_load_string($postArr, 'SimpleXMLElement', LIBXML_NOCDATA);
-        //$postObj->ToUserName = '';
-        //$postObj->FromUserName = '';
-        //$postObj->CreateTime = '';
-        //$postObj->MsgType = '';
-        //$postObj->Event = '';
-        // gh_e79a177814ed
-        //判断该数据包是否是订阅的事件推送
-/*        if (strtolower($postObj->MsgType) == 'event') {
-            //如果是关注 subscribe 事件
-            if (strtolower($postObj->Event == 'subscribe')) {
-                //回复用户消息(纯文本格式)
+        if($postArr) {
+            $postObj = simplexml_load_string($postArr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            //$postObj->ToUserName = '';
+            //$postObj->FromUserName = '';
+            //$postObj->CreateTime = '';
+            //$postObj->MsgType = '';
+            //$postObj->Event = '';
+            // gh_e79a177814ed
+            //判断该数据包是否是订阅的事件推送
+            /*        if (strtolower($postObj->MsgType) == 'event') {
+                        //如果是关注 subscribe 事件
+                        if (strtolower($postObj->Event == 'subscribe')) {
+                            //回复用户消息(纯文本格式)
+                            $toUser = $postObj->FromUserName;
+                            $fromUser = $postObj->ToUserName;
+                            $time = time();
+                            $msgType = 'text';
+                            $content = '欢迎关注我们的微信公众账号' . $postObj->FromUserName . '-' . $postObj->ToUserName;
+                            $template = "<xml>
+                                        <ToUserName><![CDATA[%s]]></ToUserName>
+                                        <FromUserName><![CDATA[%s]]></FromUserName>
+                                        <CreateTime>%s</CreateTime>
+                                        <MsgType><![CDATA[%s]]></MsgType>
+                                        <Content><![CDATA[%s]]></Content>
+                                        </xml>";
+                            $info = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+                            echo $info;
+                        }
+                    }*/
+//用户发送tuwen1关键字的时候，回复一个单图文
+            if (strtolower($postObj->MsgType) == 'text' && trim($postObj->Content) == 'tuwen2') {
                 $toUser = $postObj->FromUserName;
                 $fromUser = $postObj->ToUserName;
-                $time = time();
-                $msgType = 'text';
-                $content = '欢迎关注我们的微信公众账号' . $postObj->FromUserName . '-' . $postObj->ToUserName;
+                $arr = array(
+                    array(
+                        'title' => 'imooc',
+                        'description' => "imooc is very cool",
+                        'picUrl' => 'http://www.imooc.com/static/img/common/logo.png',
+                        'url' => 'http://www.imooc.com',
+                    ),
+                    array(
+                        'title' => 'hao123',
+                        'description' => "hao123 is very cool",
+                        'picUrl' => 'https://www.baidu.com/img/bdlogo.png',
+                        'url' => 'http://www.hao123.com',
+                    ),
+                    array(
+                        'title' => 'qq',
+                        'description' => "qq is very cool",
+                        'picUrl' => 'http://www.imooc.com/static/img/common/logo.png',
+                        'url' => 'http://www.qq.com',
+                    ),
+                );
                 $template = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							</xml>";
-                $info = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
-                echo $info;
-            }
-        }*/
-//用户发送tuwen1关键字的时候，回复一个单图文
-        if( strtolower($postObj->MsgType) == 'text' && trim($postObj->Content)=='tuwen2' ){
-            $toUser = $postObj->FromUserName;
-            $fromUser = $postObj->ToUserName;
-            $arr = array(
-                array(
-                    'title'=>'imooc',
-                    'description'=>"imooc is very cool",
-                    'picUrl'=>'http://www.imooc.com/static/img/common/logo.png',
-                    'url'=>'http://www.imooc.com',
-                ),
-                array(
-                    'title'=>'hao123',
-                    'description'=>"hao123 is very cool",
-                    'picUrl'=>'https://www.baidu.com/img/bdlogo.png',
-                    'url'=>'http://www.hao123.com',
-                ),
-                array(
-                    'title'=>'qq',
-                    'description'=>"qq is very cool",
-                    'picUrl'=>'http://www.imooc.com/static/img/common/logo.png',
-                    'url'=>'http://www.qq.com',
-                ),
-            );
-            $template = "<xml>
 						<ToUserName><![CDATA[%s]]></ToUserName>
 						<FromUserName><![CDATA[%s]]></FromUserName>
 						<CreateTime>%s</CreateTime>
 						<MsgType><![CDATA[%s]]></MsgType>
-						<ArticleCount>".count($arr)."</ArticleCount>
+						<ArticleCount>" . count($arr) . "</ArticleCount>
 						<Articles>";
-            foreach($arr as $k=>$v){
-                $template .="<item>
-							<Title><![CDATA[".$v['title']."]]></Title> 
-							<Description><![CDATA[".$v['description']."]]></Description>
-							<PicUrl><![CDATA[".$v['picUrl']."]]></PicUrl>
-							<Url><![CDATA[".$v['url']."]]></Url>
+                foreach ($arr as $k => $v) {
+                    $template .= "<item>
+							<Title><![CDATA[" . $v['title'] . "]]></Title> 
+							<Description><![CDATA[" . $v['description'] . "]]></Description>
+							<PicUrl><![CDATA[" . $v['picUrl'] . "]]></PicUrl>
+							<Url><![CDATA[" . $v['url'] . "]]></Url>
 							</item>";
-            }
+                }
 
-            $template .="</Articles>
+                $template .= "</Articles>
 						</xml> ";
-            ob_clean();
-            echo sprintf($template, $toUser, $fromUser, time(), 'news');
+                ob_clean();
+                echo sprintf($template, $toUser, $fromUser, time(), 'news');
 
-            //注意：进行多图文发送时，子图文个数不能超过10个
-        }else{
-            switch( trim($postObj->Content) ){
-                case 1:
-                    $content = '您输入的数字是1';
-                    break;
-                case 2:
-                    $content = '您输入的数字是2';
-                    break;
-                case 3:
-                    $content = '您输入的数字是3';
-                    break;
-                case 4:
-                    $content = "<a href='http://www.imooc.com'>慕课</a>";
-                    break;
-                case '英文':
-                    $content = 'imooc is ok';
-                    break;
-            }
-            $template = "<xml>
+                //注意：进行多图文发送时，子图文个数不能超过10个
+            } else {
+                switch (trim($postObj->Content)) {
+                    case 1:
+                        $content = '您输入的数字是1';
+                        break;
+                    case 2:
+                        $content = '您输入的数字是2';
+                        break;
+                    case 3:
+                        $content = '您输入的数字是3';
+                        break;
+                    case 4:
+                        $content = "<a href='http://www.imooc.com'>慕课</a>";
+                        break;
+                    case '英文':
+                        $content = 'imooc is ok';
+                        break;
+                }
+                $template = "<xml>
 <ToUserName><![CDATA[%s]]></ToUserName>
 <FromUserName><![CDATA[%s]]></FromUserName>
 <CreateTime>%s</CreateTime>
@@ -138,16 +138,16 @@ class WxController extends Controller
 <Content><![CDATA[%s]]></Content>
 </xml>";
 //注意模板中的中括号 不能少 也不能多
-            $fromUser = $postObj->ToUserName;
-            $toUser   = $postObj->FromUserName;
-            $time     = time();
-            // $content  = '18723180099';
-            $msgType  = 'text';
-            ob_clean();
-            echo sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+                $fromUser = $postObj->ToUserName;
+                $toUser = $postObj->FromUserName;
+                $time = time();
+                // $content  = '18723180099';
+                $msgType = 'text';
+                ob_clean();
+                echo sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
 
+            }
         }
-
 
     }
     private function checkSignature()
